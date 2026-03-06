8fc8d203aaa58d766fbb930306e046764dd63314
Martin2197 <martin.villalba2197@gmail.com>
2026-03-06 18:58:02 -0300

    Move projects to data file

--- server.js
+++ server.js
@@ -6,24 +6,7 @@ const PORT = 3000;
 
 app.use(cors());
 
-const projects = [
-  {
-    name: "Landing MasStore",
-    description: "Landing de productos electrónicos",
-    image: "https://martin2197.github.io/Martin-portfolio/ASSETS/Landing-MasStore.png",
-    tech: ["HTML", "CSS", "JS"],
-    demo: "https://martin2197.github.io/Martin-portfolio/",
-    github: "https://github.com/Martin2197"
-  },
-  {
-    name: "Maquetado Facebook",
-    description: "Réplica de Facebook para practicar Frontend",
-    image: "https://martin2197.github.io/Martin-portfolio/ASSETS/Maquetado-Facebook.png",
-    tech: ["HTML", "CSS"],
-    demo: "#",
-    github: "https://github.com/Martin2197"
-  }
-];
+const projects = require("./data/projects");
 
 app.get("/projects", (req, res) => {
   res.json(projects);
