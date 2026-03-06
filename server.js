56edbb0229204308c9d0b25a63bd5bd4d08c4d12
Martin2197 <martin.villalba2197@gmail.com>
2026-03-06 18:21:01 -0300

    Add images and tech stack to projects

--- server.js
+++ server.js
@@ -10,12 +10,16 @@ const projects = [
   {
     name: "Landing MasStore",
     description: "Landing de productos electrónicos",
+    image: "https://martin2197.github.io/Martin-portfolio/ASSETS/Landing-MasStore.png",
+    tech: ["HTML", "CSS", "JS"],
     demo: "https://martin2197.github.io/Martin-portfolio/",
     github: "https://github.com/Martin2197"
   },
   {
     name: "Maquetado Facebook",
     description: "Réplica de Facebook para practicar Frontend",
+    image: "https://martin2197.github.io/Martin-portfolio/ASSETS/Maquetado-Facebook.png",
+    tech: ["HTML", "CSS"],
     demo: "#",
     github: "https://github.com/Martin2197"
   }
